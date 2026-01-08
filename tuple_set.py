8f6b80431182b9ce504dd25e9a54605f62286ddf
Vihaan-Bhardwaj <vbkg15@gmail.com>
2026-01-08 17:18:04 +0000

    Set and tuples

---
+++ tuple_set.py
@@ -0,0 +1,40 @@
+"""t = {"SGW Contracts 2", 165, "Halo"}
+print(t)
+print(type(t))
+
+t.add("Fortnite")
+print(t)
+
+t.add(67)
+t.add(True)
+print(t)
+
+t.discard(165)
+print(t)
+
+
+
+s = {"SGW Contracts 2", 165, "Hello", 67}
+
+s.add(89)
+print(t.difference(s))
+print(t.intersection(s))
+print(t.union(s))
+t.update(s)
+print(t)"""
+
+#Tuples
+
+
+
+
+t = ("Vihaan", "SGW Contracts 2", 67,67, 8.9)
+
+print(t, type(t))
+print(t.count(67))
+print(t.index(67))
+t[0] = "VB"
+print(t)
+
+
+
