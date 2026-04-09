88a5cc38a71343c31b1f20954d0e0108cc1cadb9
Vihaan-Bhardwaj <vbkg15@gmail.com>
2026-04-09 18:03:53 +0100

    Add files via upload

---
+++ constant_time_calculation.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration = 0
+    print(f"The number entered by the user is {n}")
+    iteration += 1
+    print(f"The number of iterations it took was {iteration}")
+
+printnumber(67)
+printnumber(10)
+print("\nNo matter how big the localvar n is in the function printnumber(n), there will always be 1 iteration")
