a5cd0417bfee9b0f4bdcb913d285c016d88b1546
Vihaan-Bhardwaj <vbkg15@gmail.com>
2026-04-09 18:15:55 +0100

    Add files via upload

---
+++ linear_complexity.py
@@ -0,0 +1,10 @@
+def ontime(n):
+    iteration = 0
+    for i in range(1, n+1):
+        iteration += 1
+    print("When i is" , n, "iteration =" , iteration)
+
+ontime(67)
+ontime(10)
+ontime(123)
+print("\nWith every 'n' the time taken by this python/java/c(+(+)) file and iterations will increase")
