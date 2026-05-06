e9b6a1acdcb55ad8aec5e44ba49bf256511404ef
Vihaan-Bhardwaj <vbkg15@gmail.com>
2026-05-06 16:39:51 +0100

    Add files via upload

---
+++ primesieve.py
@@ -0,0 +1,14 @@
+def sieve(num):
+    prime = [True for i in range(num+1)]
+    p = 2
+    while (p * p <= num):
+        if (prime[p] == True):
+            for i in range(p*p, num+1, p):
+                prime[i] = False
+        p += 1
+    for p in range(2, num +1):
+        if prime[p]:
+            print(p)
+num = int(input("Enter a number: "))
+print(f"The following numbers are the prime numbers smaller than {num} (the number you entered)")
+sieve(num)
