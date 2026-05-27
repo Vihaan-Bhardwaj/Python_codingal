df3d38abbcd9c8de9be51338aa68cd371a1e3903
Vihaan-Bhardwaj <vbkg15@gmail.com>
2026-05-27 16:50:32 +0100

    67

---
+++ 2numsthatareoddacctoscience.py
@@ -0,0 +1,21 @@
+def odd2(arr, size):
+    xorof2 = arr[0]
+    x = 0
+    y = 0
+    Setbit = 0
+    for i in range(1, size):
+        xorof2 = xorof2 ^ arr[i]
+    setbit = xorof2 & ~(xorof2 - 1)
+    for i in range(size):
+        if (arr[i] & setbit):
+            x = x ^ arr[i]
+        else:
+            y = y ^ arr[i]
+        print(f"The two odd elements are {x} and {y}")
+arr = []
+arsize = int(input("ENter array size: "))
+for i in range(0, arsize):
+    z = int(input("ENter element: "))
+    arr.append(z)
+
+odd2(arr, arsize)
